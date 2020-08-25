037cc3b8d51bcc313ad62a67de13a5bb5c4753d1
Faisalhasan7 <64736773+Faisalhasan7@users.noreply.github.com>
2020-08-25 23:51:53 +0600

    Movr to MVR
    update v1.3
    smooth scroll css removed
    added:
    back to top
    button redesign
    js codes
    updated navbar
    etc

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@ $(function () {
         } else {
             $('.navbar').removeClass('navauto');
         }
-        if (scrolling > 200) {
+        if (scrolling > 250) {
             $('.back-to-top').fadeIn(500);
         } else {
             $('.back-to-top').fadeOut(500);
@@ -47,39 +47,27 @@ $(function () {
         ]
     });
 
+    //4. Back to Top    
+    $('.back-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500);
+    });
+
+    // //5. Animation scroll js
+    // var html_body = $('html, body');
+    // $('.navbar a').on('click', function () {
+    //     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    //         var target = $(this.hash);
+    //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //         if (target.length) {
+    //             html_body.animate({
+    //                 scrollTop: target.offset().top - 50
+    //             }, 1500, );
+    //             return false;
+    //         }
+    //     }
+    // });
 
 
 });
-console.clear();
-// set the body to full height
-// document.body.style.height = `${innerHeight}px`
-
-// Range Slider Properties.
-// Fill : The trailing color that you see when you drag the slider.
-// background : Default Range Slider Background
-const sliderProps = {
-    fill: "#0B1EDF",
-    background: "rgba(255, 255, 255, 0.214)",
-};
-
-// Selecting the Range Slider container which will effect the LENGTH property of the password.
-const slider = document.querySelector(".range__slider");
-
-// Text which will show the value of the range slider.
-const sliderValue = document.querySelector(".length__title");
-
-// Using Event Listener to apply the fill and also change the value of the text.
-slider.querySelector("input").addEventListener("input", event => {
-    sliderValue.setAttribute("data-length", event.target.value);
-    applyFill(event.target);
-});
-// Selecting the range input and passing it in the applyFill func.
-applyFill(slider.querySelector("input"));
-// This function is responsible to create the trailing color and setting the fill.
-function applyFill(slider) {
-    const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
-    const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
-			0.1}%)`;
-    slider.style.background = bg;
-    sliderValue.setAttribute("data-length", slider.value);
-}
